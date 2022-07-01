97701c5f398acff8cb7e95e60953ee0f303a7eb6
Bryce S <brycebox@gmail.com>
2022-07-01 16:52:02 -0600

    fix script

--- scripts/preclude_home_network.sh
+++ scripts/preclude_home_network.sh
@@ -6,11 +6,11 @@ if grep -Fxq "$DROPPED_NETWORKS" $FILE_TO_CHANGE
 then
     echo "doing nothing"
 else
-    sed -i'.bak' '5i\
+    sed -i '5i\
     PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -I FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\
     PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\
     # drop local network requests
     ' $FILE_TO_CHANGE
 
-    sed -i'.bak' "s/DROPPED_NETWORKS/$DROPPED_NETWORKS/g" $FILE_TO_CHANGE
+    sed -i "s/DROPPED_NETWORKS/$DROPPED_NETWORKS/g" $FILE_TO_CHANGE
 fi
