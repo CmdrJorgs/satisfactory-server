1e3b2f197eae65d88f66a5b4777c5a009cc52b5b
thelinguist <brycebox@gmail.com>
2022-07-01 23:16:20 +0000

    bash scripts are dumb

--- scripts/preclude_home_network.sh
+++ scripts/preclude_home_network.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
-
 FILE_TO_CHANGE="/config/wg0.conf"
 
-if grep -Fxq "$DROPPED_NETWORKS" $FILE_TO_CHANGE
+if [[ $DROPPED_NETWORKS ]]
 then
-    echo "doing nothing"
-else
-    sed -i '5i\
-    PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -I FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\
-    PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\
-    # drop local network requests
-    ' $FILE_TO_CHANGE
+  if grep -Fq "$DROPPED_NETWORKS" $FILE_TO_CHANGE
+  then
+      echo "no networks to add to dropped networks"
+  else
+      sed '/PostUp/d' $FILE_TO_CHANGE
+      sed '/PostDown/d' $FILE_TO_CHANGE
+      sed -i '5i\
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -I FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -i %i -d DROPPED_NETWORKS -j DROP; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\
+# drop local network requests
+' $FILE_TO_CHANGE
 
-    sed -i "s/DROPPED_NETWORKS/$DROPPED_NETWORKS/g" $FILE_TO_CHANGE
+      sed -i "s@DROPPED_NETWORKS@$DROPPED_NETWORKS@g" $FILE_TO_CHANGE
+  fi
 fi
+exit 0
